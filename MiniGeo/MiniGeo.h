6fe639dfc7199bf9d3b732e7b3f5f9731aa95ec7
Christian Rühl <ruehl@objectix.de>
2018-02-15 15:08:15 +0100

    Initial commit

---
+++ MiniGeo/MiniGeo.h
@@ -0,0 +1,19 @@
+//
+//  MiniGeo.h
+//  MiniGeo
+//
+//  Created by Christian Rühl on 15.02.18.
+//  Copyright © 2018 Objectix Software Solutions GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MiniGeo.
+FOUNDATION_EXPORT double MiniGeoVersionNumber;
+
+//! Project version string for MiniGeo.
+FOUNDATION_EXPORT const unsigned char MiniGeoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MiniGeo/PublicHeader.h>
+
+
